13b274ce8796e0874cf2546a42a89583412cf665
ordnaelmedeiros <ordnaelmedeiros@gmail.com>
2019-05-17 06:57:43 -0300

    2019-05-17

---
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-topo',
+    templateUrl: './topo.component.html',
+    styleUrls: ['./topo.component.css']
+//    styles: [' .exemplo {color: red}']
+//    template: 
+//`<p>
+//    Teste teste
+//</p>`
+})
+export class TopoComponent {
+
+}
