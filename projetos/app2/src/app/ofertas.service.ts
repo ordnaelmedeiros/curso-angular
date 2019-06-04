d3c5cd9cd2eafd580e536fd591ed90da7a887b6d
ordnaelmedeiros <ordnaelmedeiros@gmail.com>
2019-06-04 06:59:08 -0300

    2019-06-04

--- projetos/app2/src/app/ofertas.service.ts
+++ projetos/app2/src/app/ofertas.service.ts
@@ -1,7 +1,27 @@
 import { Oferta } from './shared/oferta.model';
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class OfertasService {
 
+    constructor(
+        private http: Http
+    ){}
+
+    public getOfertas(): Promise<Oferta[]> {
+        return this.http.get("http://localhost:3000/ofertas?destaque=true")
+            .toPromise()
+            .then((resposta: any) => resposta.json());
+    }
+
+    public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
+        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+            .toPromise()
+            .then((resposta: any) => resposta.json());
+    }
+
+    // --- OLD ---
     private ofertas: Oferta[] = [
         {
             id: 1,
@@ -53,7 +73,7 @@ export class OfertasService {
         }
     ];
 
-    public getOfertas(): Promise<Oferta[]> {
+    public getOfertasOld(): Promise<Oferta[]> {
         return new Promise((resolve, reject) => {
             let deu_certo = true;
             if (deu_certo) {
